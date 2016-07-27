35c74b21bf5253232893a2f1207215da47b82e71
Lee Broxson <broxsonl@gmail.com>
2016-07-26 19:23:55 -0700

    Small change made to the formatting of the hours array.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 //Global Variables
-var hours = ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: '];
+var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
 var storeLocations = [];//eslint-disable-line
 
